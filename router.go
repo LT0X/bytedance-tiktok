f611c52d80c7b7eb3c3f1400c33fc396feef05a0
XTC101 <LTOX@qq.com>
2023-01-25 00:48:59 +0800

    mod1

---
+++ router.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func initRouter(r *gin.Engine) {
+
+	// public directory is used to serve static resources
+	r.Static("/static", "./public")
+
+	apiRouter := r.Group("/douyin")
+
+	// basic apis
+	apiRouter.GET("/feed/")
+	apiRouter.GET("/user/")
+	apiRouter.POST("/user/register/")
+	apiRouter.POST("/user/login/")
+	apiRouter.POST("/publish/action/")
+	apiRouter.GET("/publish/list/")
+
+	// extra apis - I
+	apiRouter.POST("/favorite/action/")
+	apiRouter.GET("/favorite/list/")
+	apiRouter.POST("/comment/action/")
+	apiRouter.GET("/comment/list/")
+
+	// extra apis - II
+	apiRouter.POST("/relation/action/")
+	apiRouter.GET("/relation/follow/list/")
+	apiRouter.GET("/relation/follower/list/")
+	apiRouter.GET("/relation/friend/list/")
+	apiRouter.GET("/message/chat/")
+	apiRouter.POST("/message/action/")
+
+}
